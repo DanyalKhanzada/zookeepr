8ce1564cef57fe6f358acf97ea10333ae2f51bcc
“Danyal <danyalkhanzada89@gmail.com>
2021-06-21 15:58:40 -0400

    front-end completed

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+  app.use(express.static('public'));
    // parse incoming string or array data
-   app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true }));
    // parse incoming JSON data
-   app.use(express.json());
+  app.use(express.json());
 
 
 function filterByQuery(query, animalsArray) {
@@ -113,6 +114,21 @@ function filterByQuery(query, animalsArray) {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(3001, () => {
     console.log(`API server now on port ${PORT}!!`);
